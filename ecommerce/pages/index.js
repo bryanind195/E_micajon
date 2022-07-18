6a8c1b18324f171fa75d9037b3bc8441655246bf
Angel ind <bryanind195@gmail.com>
2022-07-18 02:56:13 -0500

    la homepage va tomando forma se agrega imagenes y se agrega las variables insertadas del producto cargado en el dashboard de sanity todo OK 54:15 min

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -5,8 +5,8 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
-    <HeroBanner   />
-    {console.log(bannerData)}
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    
     <div className="products-heading">
       <h2>Best Seller Products</h2>
       <p>speaker There are many variations passages</p>
