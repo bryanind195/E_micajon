1c8368cfbfda818cec563f5ff1fc5ff64984a7ec
Angel ind <bryanind195@gmail.com>
2022-07-25 02:29:30 -0500

    se agerga stripe este envie data pero no responde porque no funciona en ecuador necesita activar cuenta 3:10:04

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -1,7 +1,9 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    
     try {
         const params = {
             submit_type: 'pay',
@@ -9,17 +11,30 @@ export default async function handler(req, res) {
             payment_method_types: ['card'],
             billing_address_collection: 'auto',
             shipping_options:[
-                {shipping_rate: 'shr_1LP8AVJyeUcPpXdK7GsVDxgv' },
-                {shipping_rate: 'shr_1LP8DOJyeUcPpXdKZRsRRhF1' },
+                {shipping_rate: 'sshr_1LPKmGJyeUcPpXdKunxT99TE' },
+                
             ],
-            line_items: [
-                {
+            line_items: req.body.map((item) => {
+                const img = item.image[0].asset._ref;
+                const newImage = img.replace('image-', 'https://cdn.sanity.io/images/rp9kzczf/production/').replace('-webp', '.webp');
 
-                    price: '{{PRICE_ID}}',
-                    quantity: 1,
-                },
-            ],
-            mode: 'payment',
+                return {
+                  price_data: { 
+                    currency: 'usd',
+                    product_data: { 
+                      name: item.name,
+                      images: [newImage],
+                    },
+                    unit_amount: item.price * 100,
+                    },
+                    adjustable_quantity: {
+                      enabled:true,
+                      minimum: 1,
+                    },
+                    quantity: item.quantity
+                  }
+            } ),
+            
             success_url: `${req.headers.origin}/?success=true`,
             cancel_url:`${req.headers.origin}/?canceled=true `,
         }
@@ -27,7 +42,8 @@ export default async function handler(req, res) {
 
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
